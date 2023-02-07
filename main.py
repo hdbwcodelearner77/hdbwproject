ee6bcb290c67ff509f00cc31e3f2addfa51a6efc
hdbwcodelearner77 <124680967+hdbwcodelearner77@users.noreply.github.com>
2023-02-07 13:28:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 import random
-
+###ich habe die kommentare nach belieben auf deutsch und englisch verfasst nicht wundern ;)###
 #pygame initialisieren mit init
 pygame.init()
 
@@ -43,16 +43,16 @@ ground = pygame.transform.scale(ground,(window_width, ground.get_height()))
 player_down = pygame.transform.scale2x(pygame.image.load('player/yellowbird-downflap.png').convert())
 player_mid = pygame.transform.scale2x(pygame.image.load('player/yellowbird-midflap.png').convert())
 player_up = pygame.transform.scale2x(pygame.image.load('player/yellowbird-upflap.png').convert())
-#player setup
+#player list for animation frames
 player_animation = [player_up, player_mid, player_down]
 player = player_animation[0]
 player_rect = player.get_rect(center=(100,400))
 
-#player userevent
+#create player userevent for animation
 ANIMATIONEVENT = pygame.USEREVENT
 pygame.time.set_timer(ANIMATIONEVENT, 100)
 player_counter = 0
-#obstacle userevent
+#obstacle userevent 
 SPAWNOBSTACLE = pygame.USEREVENT + 1
 pygame.time.set_timer(SPAWNOBSTACLE, 800)
 
@@ -60,117 +60,134 @@ pygame.time.set_timer(SPAWNOBSTACLE, 800)
 obstacle = pygame.transform.scale2x(pygame.image.load('textures/pipe.png').convert())
 #create random obstacle list
 obstacle_choices = [700,800,900,1000]
-
+#obstacles array
 obstacles = [0]
 
 #set counter 0
 counter = 0
 
+#function for score
 def score(counter, obstacles):
     text = "Score:"
+    #add counter to score
     text += str(int(counter / 2))
-
+    #render and blit score
     score = font.render(text, True, (255, 255, 255))
     window.blit(score, (150, 50))
-
+    
     for obstacle in obstacles:
+        #check if player passes obstacles 
         if player_rect.centerx == obstacle.centerx:
             counter += 1
             pygame.mixer.Sound.play(coin_sound)
     return counter
 
 
-
+#function for player collision check with ground and obstacles
 def collisions(obstacles, player_rect):
+    #check ground collision
     if player_rect.centery > 720:
         pygame.mixer.Sound.play(hit_sound)
         return True
 
     for obstacle in obstacles:
+        #check obstacle collision
         if player_rect.colliderect(obstacle):
             pygame.mixer.Sound.play(hit_sound)
             return True
 
     return False
 
-
+#set obstacle position normal and flipped
 def spawn_obstacle():
     obstacle_height = random.choice(obstacle_choices)
     obstacle_rect_normal = obstacle.get_rect(center = (700, obstacle_height))
     obstacle_rect_flip = obstacle.get_rect(center = (700, obstacle_height - 900))
     return(obstacle_rect_normal, obstacle_rect_flip)
 
-
+#create obstacles in game 
 def draw_obstacles(obstacles):
     for obstacle_x in obstacles:
         if obstacle_x.bottom >= 800:
             window.blit(obstacle, obstacle_x)
         else:
+            #flip obstacles                                #flip auf x-achse = False flip auf y-achse = true
             new_obstacle = pygame.transform.flip(obstacle, False, True)
             window.blit(new_obstacle, obstacle_x)
 
-
+#obstacles auf spieler zukommen lassen
 def move_obstacles(obstacles):
     for obstacle in obstacles:
         obstacle.centerx -= 4
 
 
-#x-achse des hintergrund
+#x-achse des hintergrund global festlegen
 ground_x = 0
 backround_x = 0
-
+#collision wird auf true gesetzt damit beim starten des spiels die bedingungen unten im loop erfüllt werden (starterscreen etc.)
 collision = True
 
-#while-schleife (gameloop)
+#while-schleife (gameloop, MainPart)
 while True:
+    #Quit event einbauen 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        #spacebar keydown event for flying ability 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 pygame.mixer.Sound.play(wing_sound)
                 player_move = 0
                 player_move -= 7
             collision = False
-
+        #userevent for obstacles spawn
         if event.type == SPAWNOBSTACLE:
             obstacles.extend(spawn_obstacle())
-
+        #userevent for player wing animation
         if event.type == ANIMATIONEVENT:
             player = player_animation[player_counter % 3]
             player_counter += 1
 
-
+    #.blit zum zeichen vom background und zweiten identischen background direkt daneben ausßerhalb des bildes
     window.blit(backround, (backround_x,0))
     window.blit(backround, (backround_x + window_width,0))
     backround_x -=0.5
+    #endlosschleife für background animation
     if abs(backround_x) == window_width:
+        #sobald der duplizierte background das ganze bild erreicht wird er von neu von rechts nach links geschoben
         backround_x = 0
-
+    #solange die collision false ist wird alles ausgeführt damit das spiel läuft
     if collision is not True:
         player_move += 0.25
+        #rectangle des players den player_move zuweisen damit alles synchron ist für die collision control
         player_rect.centery += player_move
+        #player bekommt physic funktion damit er sich je nach gravitation in die richtige richtung dreht und der "newplayer" wird geblitet
         newplayer = pygame.transform.rotate(player, player_move * -5)
         window.blit(newplayer, player_rect)
         draw_obstacles(obstacles)
         move_obstacles(obstacles)
         collision = collisions(obstacles, player_rect)
         counter = score(counter, obstacles)
-
+    #sobald die collision true ist werden folgende dinge ausgeführt
     else:
+        #obstacles verschwinden
         obstacles = []
+        #counter wird auf 0 gesetzt
         counter = 0
+        #player anfangsposition wird vorgegeben
         player_rect.centery = 500
+        #starter screen wird geblittet(angezeigt)
         window.blit(startscreen, (40,50))
 
-
+    #der ground wird als letztes geblittet damit er alles andere überdeckt
+    #selbe methode wie beim backgound um animation zu hinzuzufügen
     window.blit(ground, (ground_x, 720))
     window.blit(ground, (ground_x + window_width, 720))
     ground_x -= 1.5
     if abs(ground_x) == window_width:
         ground_x = 0
-
+    #dem clock element wird mit .tick unsere refresh_rate (120) festgelegt
     FPS.tick(refresh_rate)
+    #update function damit bild sich konntinuierlich aktualisiert
     pygame.display.update()
